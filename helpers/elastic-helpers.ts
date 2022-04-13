530e581dc54118721ebe406ddedbedf8112ad42e
Sebastian <sa224ny@student.lnu.se>
2022-04-13 14:43:03 +0200

    Add: loadClient method to create an elasticsearch client

---
+++ helpers/elastic-helpers.ts
@@ -0,0 +1,33 @@
+import { Client } from "@elastic/elasticsearch"
+import fs from 'fs-extra'
+
+export const loadClient = () => {
+  const devOps: Object = {
+    node: process.env.ELASTIC_DEVELOPMENT_URL!,
+    auth: {
+      username: process.env.ELASTIC_USERNAME!,
+      password: process.env.ELASTIC_PASSWORD!
+    },
+    tls: {
+      ca: fs.readFileSync(process.env.CERT_PATH!),
+      rejectUnauthorized: false
+    }
+  }
+
+  const prodOps: Object  = {
+    node: process.env.ELASTIC_PRODUCTION_URL!,
+    auth: {
+      username: process.env.ELASTIC_USERNAME!,
+      password: process.env.ELASTIC_PASSWORD!
+    },
+    tls: {
+      ca: fs.readFileSync(process.env.CERT_PATH!),
+      rejectUnauthorized: false
+    }
+  }
+
+  const options: Object = process.env.NODE_ENV === 'production' ? prodOps : devOps
+
+  const client: Client = new Client(options)
+  return client
+}
