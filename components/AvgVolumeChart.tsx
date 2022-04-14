a312c884120e9122069c6a94aba689e55fc96228
Sebastian <sa224ny@student.lnu.se>
2022-04-14 12:34:11 +0200

    Refactor: Removed call to api

--- components/AvgVolumeChart.tsx
+++ components/AvgVolumeChart.tsx
@@ -1,31 +1,22 @@
-import axios from 'axios'
 import dynamic from 'next/dynamic'
 import React from 'react'
 import styles from './styles/AvgVolumeChart.module.css'
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
-function AvgVolumeChart(context: any) {
-  const [avgVolData, setAvgVolData] = React.useState([])
-  const [volTraded, setVolTraded] = React.useState([])
-  const [optAvgVol, setOptAvgVol] = React.useState({})
 
+function AvgVolumeChart(context: any) {
+  const [avgVolData, setAvgVolData] = React.useState<object[]>([])
+  const [volTraded, setVolTraded] = React.useState<number[]>([])
+  const [optAvgVol, setOptAvgVol] = React.useState({})
   React.useEffect(() => {
-    const loadSumVol = async () => {
-      const sumVolDataResponse: any = await axios.get(`/api/update-data`)
-      setAvgVolData(sumVolDataResponse.data.avgVolume)
-    }
-    if (avgVolData.length === 0) {
-      try {
-        loadSumVol()
-      } catch (e) {
-        console.log(e)
-      }
+    if (context.chartData) {
+      setAvgVolData(context.chartData.buckets)
     }
   }, [])
 
   React.useEffect(() => {
     if (avgVolData.length > 0) {
-      let tradedVolume: any = []
-      let symbols: any = []
+      let tradedVolume: number[] = []
+      let symbols: string[] = []
       avgVolData.forEach((item: any) => {
         tradedVolume.push(item['1'].value)
         symbols.push(item.key)
