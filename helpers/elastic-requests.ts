c5a5d85c1bd3105d04288cd41be60992205468be
Sebastian <sa224ny@student.lnu.se>
2022-04-14 12:36:02 +0200

    Refactor: Removed single query requests and added getData

--- helpers/elastic-requests.ts
+++ helpers/elastic-requests.ts
@@ -1,12 +1,9 @@
 import { Client } from "@elastic/elasticsearch"
 import { loadClient } from "./elastic-helpers"
-import requests from './req-list'
+import queryBody from './req-list'
 
-
-export const getSumVolume = async () => {
+export const getData = async () => {
   const client: Client = loadClient()
-  const reqBody = requests.sumVolume
-
   try {
     type SearchOptions = {
       index: string
@@ -15,38 +12,14 @@ export const getSumVolume = async () => {
 
     const searchOptions: SearchOptions = {
       index: 'stocksdata',
-      body: reqBody
+      body: queryBody
     }
     const result: any = await client.search(searchOptions)
     if (result) {
-      return result.aggregations['0'].buckets
+      return result.aggregations
     }
     
-  } catch (err) {
-    throw new Error('Ops! Something went wrong, please try again later.')
-  }
-}
-
-export const getAvgVolume = async () => {
-  const client: Client = loadClient()
-  const reqBody = requests.avgVolume
-
-  try {
-    type SearchOptions = {
-      index: string
-      body: any
-    }
-
-    const searchOptions: SearchOptions = {
-      index: 'stocksdata',
-      body: reqBody
-    }
-    const result: any = await client.search(searchOptions)
-    if (result) {
-      return result.aggregations['0'].buckets
-    }
-    
-  } catch (err) {
-    throw new Error('Ops! Something went wrong, please try again later.')
+  } catch (err: any) {
+    throw new Error(err.message)
   }
 }
