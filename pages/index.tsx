1d9ec2a24df4e167af1cf0768b9b6b51e5ec4fe2
Sebastian <sa224ny@student.lnu.se>
2022-04-14 12:31:16 +0200

    Refactor: index page to call the api instead of each chart component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
 import Head from 'next/head'
+import React from 'react'
 import AvgVolumeChart from '../components/AvgVolumeChart'
 import SumVolumeChart from '../components/SumVolumeChart'
 import styles from '../styles/Home.module.css'
+
+type AggResponse = {
+  data: object
+}
+
+type ChartData = {
+  avg_volume: object
+  sum_volume: object
+}
+
 function Home() {
+  const [loaded, setLoaded] = React.useState(false)
+  const [data, setData] = React.useState<ChartData | undefined>(undefined)
+  const [noData, setNoData] = React.useState(false)
+  React.useEffect(() => {
+    if (!loaded) {
+      const loadData = async () => {
+        const response: Response = await fetch('/api/get-all')
+        if (response.status !== 200) {
+          setNoData(true)
+          return
+        }
+        const dataResp: AggResponse = await response.json()
+        setData(dataResp.data as ChartData)
+        setLoaded(true)
+      }
+      loadData()
+    }
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +42,18 @@ function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Stock market analysis</h1>
-        <AvgVolumeChart />
-        <SumVolumeChart />
+        {!noData && data && data.avg_volume && (
+          <>
+            <AvgVolumeChart chartData={data.avg_volume} />
+            <SumVolumeChart chartData={data.sum_volume} />
+          </>
+        )}
+
+        {noData && (
+          <>
+            <h1>Problem reaching elastic... No data found</h1>
+          </>
+        )}
       </main>
     </div>
   )
